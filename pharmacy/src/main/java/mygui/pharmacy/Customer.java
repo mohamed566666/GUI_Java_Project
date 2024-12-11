283ba78b8e7df533c94f1bbe39df627b366b09b7
Mohamed Gamal <115277088+mohamed566666@users.noreply.github.com>
2024-12-11 22:57:53 +0200

    Add files via upload

---
+++ pharmacy/src/main/java/mygui/pharmacy/Customer.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mygui.pharmacy;
+import java.util.*;
+/**
+ *
+ * @author Mohamed
+ */
+public class Customer {
+    private String name;
+    private String customerId;
+    private String phone;
+
+    // constructor
+    public Customer(String name, String customerId, String phone) {
+        this.name = name;
+        this.customerId = customerId;
+        this.phone = phone;
+    }
+
+    // method to get the name of this customer
+    public String getname() {
+        return name;
+    }
+
+    // method to get ID of this customer
+    public String getcustomerId() {
+        return customerId;
+    }
+
+    // method to get Phone Number of this customer
+    public String getphone() {
+        return phone;
+    }
+
+    // method to display All details of this customer
+    public void display() {
+        System.out.println("name : " + getname() + "\n" + "phone : " + getphone() + "\n" + "customerId : " + getcustomerId());
+    }
+}
---
+++ pharmacy/src/main/java/mygui/pharmacy/ManageCustomerDiscount.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mygui.pharmacy;
+
+import java.util.*;
+
+/**
+ *
+ * @author Mohamed
+ */
+public class ManageCustomerDiscount {
+    private HashMap<String, Integer> frequencyForCustomer;
+
+    // constructor
+    public ManageCustomerDiscount() {
+        this.frequencyForCustomer = new HashMap<>();
+    }
+
+    // method to add customer
+    public void addCustomer(String id) {
+        frequencyForCustomer.put(id, 1);
+    }
+
+    // method to check if this customer
+    public void checkAndUpdate(String id) {
+        if (!frequencyForCustomer.containsKey(id)) {
+            addCustomer(id);
+        } else {
+            frequencyForCustomer.put(id, frequencyForCustomer.get(id) + 1);
+        }
+    }
+
+    // method to calculate the percentage of discount by using Number of visits
+    public double calculateDiscount(String id) {
+        if (!frequencyForCustomer.containsKey(id)) {
+            return 0;
+        }
+        int purchaseFrequency = frequencyForCustomer.get(id);
+        double discount;
+        if (purchaseFrequency >= 20) {
+            discount = 0.20;
+        } else if (purchaseFrequency >= 10) {
+            discount = 0.10;
+        } else if (purchaseFrequency >= 5) {
+            discount = 0.05;
+        } else {
+            discount = 0.0;
+        }
+        return discount;
+    }
+}
